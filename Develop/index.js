f85895b860946d6405f9b04c2f4d0786fe31319a
Margot Cooper Gruen <margotgruen@gmail.com>
2022-08-10 23:52:38 -0700

    packages required added to top of js file, and node commands added in comments

--- Develop/index.js
+++ Develop/index.js
@@ -1,4 +1,9 @@
 // TODO: Include packages needed for this application
+const fs = require("fs");
+const inquirer = require("inquirer");
+
+//in node type: npm init -y
+//in node type: npm i inquirer@8.2.4
 
 // TODO: Create an array of questions for user input
 const questions = [];
@@ -6,6 +11,11 @@ const questions = [];
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
 
+// fs.appendFile("log.txt", `${process.argv[2]}\n`, (err) =>
+//   // TODO: Describe how this ternary operator works
+//   //if there is an error, then console log "error", else log "Commit logged"
+//   err ? console.error(err) : console.log("Commit logged!")
+// );
 // TODO: Create a function to initialize app
 function init() {}
 
